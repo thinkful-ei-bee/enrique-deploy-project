ad9d55a63a725f07cbf8685e46d1c10e0bfef192
Enrique Montemayor <eemontemayor@live.com>
2019-03-29 11:42:05 -0500

    search form

--- star-wars-deploy/src/App.js
+++ star-wars-deploy/src/App.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import {Router} from 'react-router-dom';
+import Main from './Main';
 import './App.css';
+import CharSearch from "./CharSearch";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          
-        
-        </header>
-      </div>
-    );
+export default class App extends React.Component {
+  constructor(props) { 
+      super(props);
+      this.state = {
+        searchCharacter:[],
+        characterList:[],
+      };
   }
+
+  componentDidMount() {} 
+
+  handleCharSubmit = name => {
+    this.setState({
+      searchCharacter:[
+        ...this.state.searchCharacter,
+        name
+      ]
+    })
+  }
+
+  render() { 
+      return (<div> 
+        <header>
+          <h1>STAR WARS SEARCH</h1>
+          </header>
+          <CharSearch searchChar= {this.state.handleCharSubmit}/>
+          <Main>Results</Main>
+      </div>);
+  }
+  
 }
 
-export default App;
---
+++ star-wars-deploy/src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>
+            <h3>
+            Results
+            </h3>
+            </div>
+        )
+
+    }
+}
