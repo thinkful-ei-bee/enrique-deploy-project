df5c37946eaaf8230c0e5711e43daa53217958b9
Enrique Montemayor <eemontemayor@live.com>
2019-03-29 22:09:28 -0500

    added pseudocode

--- star-wars-deploy/src/Results.js
+++ star-wars-deploy/src/Results.js
@@ -25,3 +25,31 @@ export default function Results (props){
   
     
 }
+//TURN THIS INTO A CLASS COMP AND FOLLOW TEMPLATE BELOW FOR LOADING INDICATOR..
+//COULD PROBABLY FIND A STARWARS ONE
+
+// Loading Indicators with Ajax
+
+// class RepoList extends React.Component {
+//     state = {
+//         loading: false, // Extra state - tells us whether we are loading
+//         repos: []
+//     };
+//     componentDidMount() {
+//         this.setState({loading: true}); // AJAX request is starting
+//         fetch('https://api.github.com/users/dhh/repos')
+//             .then(res => res.json())
+//             .then(repos => this.setState({
+//                 repos,
+//                 loading: false // AJAX request is done
+//             }));
+//     }
+//     render() {
+//         // Render a message if the request is ongoing
+//         if (this.state.loading) {
+//             return <div>Loading...</div>;
+//         }
+//         const repos = this.state.repos.map(repo => <li>{repo.name}</li>);
+//         return <ul>{repos}</ul>;
+//     }
+// }
--- star-wars-deploy/src/SearchForm.js
+++ star-wars-deploy/src/SearchForm.js
@@ -4,7 +4,7 @@ import config from './config';
 export default class SearchForm extends React.Component {
  constructor(props){
    super(props);
-   this.state={name:'', spaceship:'',
+   this.state={name:'', spaceship:'', species:'', films:'', vehicles:'', planets:'',
  }
 
  }
@@ -15,6 +15,8 @@ export default class SearchForm extends React.Component {
    
     
     let url = `${config.API_ENDPOINT}${this.state.name}`;
+// make switch statement function to adjust url to search for people, spaceship, etc
+
     console.log(url)
     fetch(url)
     .then(res => res.json())
@@ -35,9 +37,14 @@ handleChange=(e)=>{
       console.log(this.props)
         return (<div> 
           <form className="SearchForm" onSubmit={this.fetchSubmit.bind(this)}>
-            <h2>Explore the Star Wars universe</h2>
-            <input type='text' name='name' onChange={this.handleChange.bind(this)}  required/>
-            <input type='text' name='spaceship' onChange={this.handleChange.bind(this)}  />
+            <h2>Search for:</h2>
+            <input type='text' name='name' placeholder='characters' onChange={this.handleChange.bind(this)}  />
+            <input type='text' name='spaceship' placeholder='spaceships' onChange={this.handleChange.bind(this)}  />
+            <input type='text' name='planet' placeholder='planets' onChange={this.handleChange.bind(this)}  />
+            <input type='text' name='vehicles' placeholder='vehicles' onChange={this.handleChange.bind(this)}  />
+            <input type='text' name='species' placeholder='species' onChange={this.handleChange.bind(this)}  />
+            <input type='text' name='films' placeholder='films' onChange={this.handleChange.bind(this)}  />
+
             <button type='submit' onClick={()=> console.log('button works')}>Search</button>
             {// could make a dropdown select menu to offer what to search for e.g. planet, people, etc 
             }
