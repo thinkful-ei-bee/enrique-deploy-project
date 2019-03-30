762dfce8a862c1099a098ac335c18e7ab33dba04
Enrique Montemayor <eemontemayor@live.com>
2019-03-30 13:19:09 -0500

    adjusted api fetch to search for non-characters

--- star-wars-deploy/src/SearchForm.js
+++ star-wars-deploy/src/SearchForm.js
@@ -4,7 +4,7 @@ import config from './config';
 export default class SearchForm extends React.Component {
  constructor(props){
    super(props);
-   this.state={name:'', spaceship:'', species:'', films:'', vehicles:'', planets:'',
+   this.state={category:'', term:''
  }
 
  }
@@ -12,11 +12,7 @@ export default class SearchForm extends React.Component {
 
   fetchSubmit = e =>{
     e.preventDefault();
-   
-    
-    let url = `${config.API_ENDPOINT}${this.state.name}`;
-// make switch statement function to adjust url to search for people, spaceship, etc
-
+    let url = `${config.API_ENDPOINT}${this.state.category}/?search=${this.state.term}`;
     console.log(url)
     fetch(url)
     .then(res => res.json())
@@ -25,28 +21,32 @@ export default class SearchForm extends React.Component {
 
 }
 
+// handleChange=(e)=>{ Jon trick
+  
+//   this.setState({[e.target.name]: e.target.value})
+// }
 handleChange=(e)=>{
   
-  this.setState({[e.target.name]: e.target.value})
+  this.setState({category: e.target.name, term: e.target.value})
 }
   //MAKE ONE FUNCTION THAT VALIDATES FOR ALL... CHECK AGAINST NUMBERS AND OTHER NON-LETTERS
-  //PROVIDE A DEFAULT MESSAGE IF INPUT RETURNS WITH NO RESULTS
+  //PROVIDE A DEFAULT MESSAGE IF INPUT RETURNS WITH NO RESULTS and trims spaces or whitespace
 
     componentDidMount() {} 
   
     render() { 
-      console.log(this.props)
+      
         return (<div> 
           <form className="SearchForm" onSubmit={this.fetchSubmit.bind(this)}>
             <h2>Search for:</h2>
-            <input type='text' name='name' placeholder='characters' onChange={this.handleChange.bind(this)}  />
-            <input type='text' name='spaceship' placeholder='spaceships' onChange={this.handleChange.bind(this)}  />
-            <input type='text' name='planet' placeholder='planets' onChange={this.handleChange.bind(this)}  />
+            <input type='text' name='people' placeholder='characters' onChange={this.handleChange.bind(this)}  />
+            <input type='text' name='starships' placeholder='starships' onChange={this.handleChange.bind(this)}  />
+            <input type='text' name='planets' placeholder='planets' onChange={this.handleChange.bind(this)}  />
             <input type='text' name='vehicles' placeholder='vehicles' onChange={this.handleChange.bind(this)}  />
             <input type='text' name='species' placeholder='species' onChange={this.handleChange.bind(this)}  />
             <input type='text' name='films' placeholder='films' onChange={this.handleChange.bind(this)}  />
 
-            <button type='submit' onClick={()=> console.log('button works')}>Search</button>
+            <button type='submit'>Search</button>
             {// could make a dropdown select menu to offer what to search for e.g. planet, people, etc 
             }
             </form>
